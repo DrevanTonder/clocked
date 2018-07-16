76c3e4071c7c6f0ed0248206ad89a75a6e64a157
Andre van Tonder <drevtonder@gmail.com>
2018-07-16 13:59:54 +0800

    add clock to home

---
+++ src/components/Clock.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="clock">
+        <time class="datetime" :datetime="time">     
+            <h1 class="time"><span class="hours">{{ hours }}</span>:<span class="minutes">{{ minutes }}</span>:<span class="seconds">{{ seconds }}</span></h1>
+
+            <h2>{{ dayAsString }}, {{ date }} {{ monthAsString }}</h2>
+        </time>
+    </div>    
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  data() {
+    return {
+      time: new Date(),
+      interval: setInterval(() => (this.time = new Date()), 1000)
+    };
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  computed: {
+    dayAsString() {
+      return moment(this.time).format("dddd");
+    },
+
+    monthAsString() {
+      return moment(this.time).format("MMMM");
+    },
+
+    year() {
+      return moment(this.time).format("YYYY");
+    },
+
+    month() {
+      return moment(this.time).format("MM");
+    },
+
+    date() {
+      return moment(this.time).format("D");
+    },
+
+    day() {
+      return moment(this.time).format("d");
+    },
+
+    hours() {
+      return moment(this.time).format("HH");
+    },
+
+    minutes() {
+      return moment(this.time).format("mm");
+    },
+
+    seconds() {
+      return moment(this.time).format("ss");
+    }
+  }
+};
+</script> 
+
+<style lang="scss">
+.clock {
+  background-color: #f8f8f8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.datetime {
+  background-color: transparent;
+  text-align: center;
+  width: 50%;
+  height: 50%;
+}
+
+.time {
+  font-size: 6rem;
+}
+
+.hours {
+  font-size: 8rem;
+}
+
+.minutes {
+  font-size: 8rem;
+}
+
+.seconds {
+  font-size: 4rem;
+}
+</style>
