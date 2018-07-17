549c828662603d46135f67fc139ea0fee4f33edb
Andre van Tonder <drevtonder@gmail.com>
2018-07-17 08:54:53 +0800

    add basic timer

--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -1,11 +1,46 @@
 <template>
     <div class="main has-text-primary">
         <div class="main-text" >
-            <h1>Timer</h1>
+            <time :datetime="timeLeft">
+                <h1 class="time is-size-1">
+                <span class="time-big">{{ timeLeft | minutes }}</span>:<span class="time-big">{{ timeLeft | seconds }}</span>:<span class="time-small">{{ timeLeft | milliseconds }}</span>
+                </h1>
+            </time>
+
+            <button aria-label="stop/start" role="button" class="button is-primary is-large is-rounded" @click="start()"><font-awesome-icon icon="play" /></button>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      timeToCountdown: null,
+      timeLeft: 0,
+      interval: null
+    };
+  },
+
+  computed: {
+    running() {
+      return this.interval != null;
+    }
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    start() {
+      this.timeToCountdown = 10000;
+
+      this.interval = setInterval(
+        () => (this.timeLeft = this.timeToCountdown - Date.now()),
+        10
+      );
+    }
+  }
+};
 </script>
